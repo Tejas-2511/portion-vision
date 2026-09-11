51ba2b528ca19c4c650e9ee2fedbe387dc5503b5
Tejas-2511 <tejastakkar2006@gmail.com>
2026-09-11 20:00:53 +0530

    check before

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-ca84f546'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.gmir0cllpmg"
+    "revision": "0.inmlc5cr66o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
